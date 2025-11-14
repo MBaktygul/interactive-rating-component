98fd44e16a91aa628ea6e57794957244aab8ad99
MBaktygul <ma.baktygul@gmail.com>
2025-11-14 19:34:30 +0100

    refactor code and make it accessible

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 let selectedRating = null;
-const ratingOptions = document.querySelectorAll('.rating-option');
+const ratingButtons = document.querySelectorAll('.rating-option-button');
 const submitButton = document.getElementById('submit');
-ratingOptions.forEach((option) => {
-  option.addEventListener('click', function () {
-    ratingOptions.forEach((opt) => opt.classList.remove('selected'));
-    option.classList.add('selected');
-    selectedRating = option.getAttribute('data-value');
+
+ratingButtons.forEach((btn) => {
+  btn.addEventListener('click', () => {
+
+    // Сбрасываем состояние у всех
+    ratingButtons.forEach(b => {
+      b.classList.remove('selected');
+      b.setAttribute('aria-checked', 'false');
+    });
+
+    // Активируем выбранную
+    btn.classList.add('selected');
+    btn.setAttribute('aria-checked', 'true');
+
+    selectedRating = btn.dataset.value;
+        console.log(`The rating ${selectedRating} chosen`);
   });
 });
-submitButton.addEventListener('click', function () {
+
+// Submit
+submitButton.addEventListener('click', () => {
   if (selectedRating) {
     localStorage.setItem('selectedRating', selectedRating);
     window.location.href = 'thank-you.html';
